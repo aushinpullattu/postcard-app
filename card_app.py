3599e7f9b310601cab0fe4b03c3406071de3f351
aushinpullattu <aushinpullattu@gmail.com>
2025-12-15 12:03:01 +0000

    Updates

--- card_app.py
+++ card_app.py
@@ -40,7 +40,7 @@ def create_postcard_high_res_small_canvas(to_name, from_name, message):
     Create postcard with smaller canvas (500x400) but crisp, big text using Patrick font.
     """
     # ---------------- High-res rendering ----------------
-    scale = 4  # render 4x bigger internally
+    scale = 400  # render 4x bigger internally
     width, height = 500*scale, 400*scale
     base = Image.new("RGBA", (width, height), (245, 240, 225))  # beige background
     draw = ImageDraw.Draw(base)
